b9274cca24ccd675ef3f084a432daea7d2f4227b
Joshua Mo <joshua.mo.876@gmail.com>
2023-07-26 09:21:03 +0100

    refactor: check midnight

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,19 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use chrono::prelude::*;
+use lettre::{
+    message::header::ContentType, transport::smtp::authentication::Credentials, AsyncSmtpTransport,
+    AsyncTransport, Message, Tokio1Executor,
+};
 use libsql_client::{client::Client, Statement};
-use reqwest::Client as ReqwestClient;
 use serde::{Deserialize, Serialize};
 use shuttle_secrets::SecretStore;
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 use tokio::sync::Mutex;
-use tokio::time::{sleep, Duration};
+use tokio::time::{sleep, Duration as TokioDuration};
+use chrono::Local;
+use chrono::naive::{NaiveDateTime, Days};
+use std::time::Duration;
 
 #[derive(Deserialize, Serialize)]
 pub struct CatFact {
@@ -22,17 +27,16 @@ pub struct CatFact {
 
 pub struct CustomService {
     db: Arc<Mutex<Client>>,
-    mg_key: String,
-    mg_url: String,
+    gmail_user: String,
+    gmail_password: String,
+    router: Router
 }
 
 pub struct AppState {
     db: Arc<Mutex<Client>>,
-    mg_key: String,
-    mg_url: String,
-    ctx: ReqwestClient,
 }
 
+#[derive(Deserialize)]
 pub struct EmailRequest {
     email: String,
 }
@@ -41,67 +45,80 @@ async fn health_check() -> impl IntoResponse {
     (StatusCode::OK, "It works!".to_string())
 }
 
+async fn homepage() -> impl IntoResponse {
+    r#"Welcome to the Cat Facts API!
+
+Here are the following routes:
+    - GET /health - Health check route.
+    - GET /catfact - Get a random cat fact.
+    - POST /catfact/create - Submit your own cat fact
+        - Takes the following JSON parameters: "fact"
+    - POST /subscribe - Subscribe to our free daily cat fact email service
+        - Takes the following JSON parameters: "email"
+"#
+}
+
 #[shuttle_runtime::main]
 async fn axum(
     #[shuttle_secrets::Secrets] store: SecretStore,
     #[shuttle_turso::Turso(addr = "{secrets.TURSO_ADDR}", token = "{secrets.TURSO_TOKEN}")]
     db: Client,
 ) -> Result<CustomService, shuttle_runtime::Error> {
-    let mg_key = store
-        .get("MAILGUN_KEY")
+    let gmail_user = store
+        .get("GMAIL_USER")
         .unwrap_or_else(|| "None".to_string());
-    let mg_url = store
-        .get("MAILGUN_URL")
+    let gmail_password = store
+        .get("GMAIL_PASSWORD")
         .unwrap_or_else(|| "None".to_string());
 
+    
+        db.batch([
+                "CREATE TABLE IF NOT EXISTS catfacts (
+        id integer primary key autoincrement,
+        fact text not null,
+        created_at datetime default current_timestamp 
+        )",
+                "CREATE TABLE IF NOT EXISTS subscribers (
+                    id integer primary key autoincrement,
+                    email text not null,
+        created_at datetime default current_timestamp 
+                )",
+            ])
+            .await
+            .unwrap();
+
+
+        let db = Arc::new(Mutex::new(db));
+    
+        let state = Arc::new(AppState {
+            db: db.clone(),
+        });
+
+        let router = Router::new()
+            .route("/", get(homepage))
+            .route("/health", get(health_check))
+            .route("/catfact", get(get_record))
+            .route("/catfact/create", post(create_record))
+            .route("/subscribe", post(subscribe))
+            .with_state(state);
+
+
     Ok(CustomService {
-        db: Arc::new(Mutex::new(db)),
-        mg_key,
-        mg_url,
+        db,
+        gmail_user,
+        gmail_password,
+        router
     })
 }
 
 #[shuttle_runtime::async_trait]
 impl shuttle_runtime::Service for CustomService {
     async fn bind(mut self, addr: std::net::SocketAddr) -> Result<(), shuttle_runtime::Error> {
-        let ctx = ReqwestClient::new();
-
-        self.db
-            .lock()
-            .await
-            .execute(
-                "CREATE TABLE IF NOT EXISTS catfacts (
-        id integer primary key autoincrement,
-        fact text not null,
-        created_at datetime default current_timestamp 
-        );
-            CREATE TABLE IF NOT EXISTS subscribers (
-                    id integer primary key autoincrement,
-                    email text not null,
-        created_at datetime default current_timestamp 
-                )",
-            )
-            .await
-            .unwrap();
-
-        let state = Arc::new(AppState {
-            db: self.db.clone(),
-            mg_key: self.mg_key.to_owned(),
-            mg_url: self.mg_url.to_owned(),
-            ctx,
-        });
-
-        let router = Router::new()
-            .route("/", get(health_check))
-            .route("/catfact", get(get_record))
-            .route("/catfact/create", post(create_record))
-            .with_state(state);
-
-        let router = axum::Server::bind(&addr).serve(router.into_make_service());
+        let router = axum::Server::bind(&addr).serve(self.router.into_make_service());
 
         tokio::select!(
             _ = router => {},
-            _ = send_mail(self.db, self.mg_key, self.mg_url) => {}
+            _ = scheduled_tasks(self.db, self.gmail_user, self.gmail_password) => {}
         );
 
         Ok(())
@@ -133,7 +150,7 @@ pub async fn create_record(
     State(state): State<Arc<AppState>>,
     Json(json): Json<CatFact>,
 ) -> Result<impl IntoResponse, impl IntoResponse> {
-    if let Err(e) = state
+    match state
         .db
         .lock()
         .await
@@ -143,33 +160,15 @@ pub async fn create_record(
         ))
         .await
     {
-        return Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string()));
+        Ok(_) => Ok(StatusCode::CREATED),
+        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
     }
-
-    Ok(StatusCode::CREATED)
 }
 
 pub async fn subscribe(
     State(state): State<Arc<AppState>>,
     Json(req): Json<EmailRequest>,
 ) -> Result<impl IntoResponse, impl IntoResponse> {
-    let api_endpoint = format!(
-        "https://api.mailgun.net/v3/lists/mail@{}/members",
-        &state.mg_url
-    );
-
-    let params = sub_params(req.email.to_owned());
-    let post = state
-        .ctx
-        .post(api_endpoint)
-        .basic_auth("api", Some(&state.mg_key))
-        .form(&params);
-
-    let _ = match post.send().await {
-        Ok(_) => Ok(StatusCode::OK),
-        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
-    };
-
     if let Err(e) = state
         .db
         .lock()
@@ -183,73 +182,84 @@ pub async fn subscribe(
         return Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string()));
     };
 
-    Ok(StatusCode::CREATED)
-}
-
-fn send_mail_params(recipient: String, fact: String) -> HashMap<&'static str, String> {
-    let mut params = HashMap::new();
-
-    params.insert("to", recipient);
-    params.insert("subject", "Your daily cat fact!".to_string());
-    params.insert("text", format!("Hey there! You're receiving an email because you're subscribed to Cat Facts, the number one source for facts about facts. \n\n Did you know? {fact}"));
-
-    params
-}
-
-fn sub_params(recipient: String) -> HashMap<&'static str, String> {
-    let mut params = HashMap::new();
-
-    params.insert("address", recipient);
-    params.insert("subscribed", "True".to_string());
-
-    params
+    Ok((StatusCode::CREATED, "You're now subscribed!".to_string()))
 }
 
 #[allow(unreachable_code)]
-pub async fn send_mail(
+pub async fn scheduled_tasks(
     db: Arc<Mutex<Client>>,
-    mg_key: String,
-    mg_url: String,
+    gmail_user: String,
+    gmail_password: String,
 ) -> Result<(), anyhow::Error> {
-    let ctx = ReqwestClient::new();
+    let creds = Credentials::new(gmail_user.to_owned(), gmail_password.to_owned());
+
+    // Open a remote connection to gmail
+    let mailer: AsyncSmtpTransport<Tokio1Executor> =
+        AsyncSmtpTransport::<Tokio1Executor>::relay("smtp.gmail.com")
+            .unwrap()
+            .credentials(creds)
+            .build();
+
+    
+    let mut tomorrow_midnight = Local::now().checked_add_days(Days::new(1)).unwrap().date_naive().and_hms_opt(0, 0, 0).unwrap();
 
     loop {
-        let time = Utc::now().format("%H:%M:%S").to_string();
-        println!("The time is: {time}");
+        let duration = calculate_time_diff(tomorrow_midnight);
+        
+        if duration == std::time::Duration::ZERO {
 
-        if time.as_str() == "00:00:00" {
-            let db = db.lock().await;
-
-            let cat_fact = match db
-                .execute("SELECT fact FROM catfacts order by random() limit 1")
-                .await
-            {
-                Ok(res) => res.rows[0].values[0].to_string(),
-                Err(e) => return Err(anyhow!("Error when trying to get a cat fact: {e}")),
-            };
-            let rows = match db.execute("SELECT email FROM subscribers").await {
-                Ok(res) => res.rows,
-                Err(e) => return Err(anyhow!("Had an error while sending emails: {e}")),
-            };
-
-            for row in rows {
-                let api_endpoint = format!("https://api.mailgun.net/v3/mail@{}/messages", mg_url);
-
-                let params = send_mail_params(row.values[0].to_string(), cat_fact.clone());
-
-                let post = ctx
-                    .post(api_endpoint)
-                    .basic_auth("api", Some(mg_key.clone()))
-                    .form(&params);
-
-                if let Err(e) = post.send().await {
-                    println!("Error while sending subscriber email: {e}")
-                };
-            }
+        send_subscriber_mail(mailer.to_owned(), db.clone()).await.expect("Looks like something went wrong trying to send subscriber mail :(");
+        
+    tomorrow_midnight = Local::now().checked_add_days(Days::new(1)).unwrap().date_naive().and_hms_opt(0, 0, 0).unwrap();
         }
-
-        let _ = sleep(Duration::from_secs(1)).await;
+        let duration = calculate_time_diff(tomorrow_midnight);
+        
+        sleep(TokioDuration::from_secs(duration.as_secs())).await;
     }
 
+    Ok(())
+}
+
+fn calculate_time_diff(midnight: NaiveDateTime) -> Duration {
+    let now = Local::now().naive_local();
+
+        midnight
+            .signed_duration_since(now)
+            .to_std()
+            .unwrap()
+}
+
+async fn send_subscriber_mail(
+    mailer: AsyncSmtpTransport<Tokio1Executor>,
+    db: Arc<Mutex<Client>>,
+) -> Result<(), anyhow::Error> {
+        let db = db.lock().await;
+
+        let cat_fact = match db
+            .execute("SELECT fact FROM catfacts order by random() limit 1")
+            .await
+        {
+            Ok(res) => res.rows[0].values[0].to_string(),
+            Err(e) => return Err(anyhow!("error when trying to get a cat fact: {e}")),
+        };
+
+        let rows = match db.execute("SELECT email FROM subscribers").await {
+            Ok(res) => res.rows,
+            Err(e) => return Err(anyhow!("Had an error while sending emails: {e}")),
+        };
+
+        for row in rows {
+            let email = Message::builder()
+                    .from("Cat Facts".parse().unwrap())
+                    .to(row.values[0].to_string().parse().unwrap())
+                    .subject("Happy new year")
+                    .header(ContentType::TEXT_PLAIN)
+                    .body(format!("Hey there! You're receiving this message because you're subscribed to Cat Facts. \n\nDid you know {cat_fact}?"))
+                    .unwrap();
+
+            if let Err(e) = mailer.send(email).await {
+                    println!("Something went wrong while sending mail: {e}")
+                }
+            }
     Ok(())
 }
